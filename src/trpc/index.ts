57f1701574a4298b7672e21f4662d1e8c80f533c
Sithija Lakshan <sithikaru@gmail.com>
2023-10-04 19:29:21 +0530

    db connected

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -8,6 +8,8 @@ export const appRouter = router({
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
     
+    const dbUser
+
     return {success: true};
   }),
 });
